a0618b6d0f0625bc9b11b2ab2f907e7418d7bf77
princerajtyagi <princerajtyagi29@gmail.com>
2025-02-03 16:52:29 +0530

    Solution to Input and Print an Integer

--- Input and Print an Integer@@6788d5012db25efec946f939/code.c
+++ Input and Print an Integer@@6788d5012db25efec946f939/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int num;
-    scanf("n", num);
-    printf(Youe entered: "%n", num);
+    scanf("%n", &num);
+    printf(You entered: "%n", num);
     return 0;
 }
