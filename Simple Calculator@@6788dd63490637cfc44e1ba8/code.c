3db8652a9c2cf73bae8709dd5882578ff2f02b69
Prince Raj Tyagi <princerajtyagi29@gmail.com>
2025-02-14 20:48:53 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,15 +4,15 @@ int main() {
     double num1, num2;
     char op;
 
-    scanf("%d %d %c", &num1, &num2, &op);
+    scanf("%lf %lf %c", &num1, &num2, &op);
 
     // Performing the operation
     if (op == '+')
-        printf("%d", num1 + num2);
+        printf("%lf", num1 + num2);
     else if (op == '-')
-        printf("%d", num1 - num2);
+        printf("Result: %.2lf\n", num1 - num2);
     else if (op == '*')
-        printf("%d", num1 * num2);
+        printf("Result: %.2lf\n", num1 * num2);
     else if (op == '/') {
         if (num2 != 0)
             printf("Result: %.2lf\n", num1 / num2);
