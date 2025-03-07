f56e5c71f3c1ccc4fbac3b8d6b227b63e53f42a4
Prince Raj Tyagi <princerajtyagi29@gmail.com>
2025-03-07 20:39:57 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to print the sorted array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main() {
     int n;
     scanf("%d", &n);
     int arr[n];
+
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    bubbleSort(arr, n);
-    printArray(arr, n);
-    return 0:
+
+    bubbleSort(arr, n); // Sort the array
+    printArray(arr, n); // Print sorted array
+
+    return 0; // Fixed the colon (:) error
 }
