cfebb73bfe3f58a8ef7d227ee660ea26577a5809
Prince Raj Tyagi <princerajtyagi29@gmail.com>
2025-04-07 16:50:36 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 
+#include <string.h>
+
+#include <ctype.h>
+
 int main() {
-    char message[100];
-    int shift, i;
 
-    printf("Enter message: ");
-    fgets(message, sizeof(message), stdin);
+char message [100];
 
-    printf("Enter shift: ");
-    scanf("%d", &shift);
+int shift;
 
-    i = 0;
-    while (message[i] != '\0') {
-        if (message[i] >= 'A' && message[i] <= 'Z') {
-            message[i] = (message[i] - 'A' + shift) % 26 + 'A';
-        } else if (message[i] >= 'a' && message[i] <= 'z') {
-            message[i] = (message[i] - 'a' + shift) % 26 + 'a';
-        }
-        i++;
-    }
+char encrypted[100]; // store
 
-    printf("Encrypted message: %s", message);
-    return 0;
+the encrypted message
+
+// Read the input message scanf("%[^\n]s", message); //
+
+Read entire line including spaces
+
+// Read the shift value
+
+scanf("%d", &shift);
+
+// Normalize the shift to the range [0, 25]
+
+shift = shift % 26;
+
+if (shift < 0) shift += 26;
+
+// Handle negative shifts
+
+// call the caesarCipher function (to be implemented by the student)
+
+caesarCipher (message, shift, encrypted);
+// Output the encrypted
+
+message
+
+printf("%s\n", encrypted);
+
+return 0;
+
 }
+
+// Implement the caesarCipher function in the solution section!
+
+You Need to Only Define The Function and Complete The Rest of The Code.
